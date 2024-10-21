f5b2f435f488c65bd10bd5c90f370beb0db8cae7
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-21 14:48:42 +0100

    chore: Set up component exports and imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/Root.jsx'
+import Header from './routes/Header.jsx';
+import Footer from './routes/Footer.jsx';
+import Homepage from './routes/Homepage.jsx';
+import Shopping from './routes/Shopping.jsx';
+import Itemcard from './routes/Itemcard.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <RouterProvider router= {router} />
   </StrictMode>,
 )
