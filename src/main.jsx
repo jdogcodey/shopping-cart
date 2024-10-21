14fbb34bd2d4e8e9bc72252d8311087bb6b20daa
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-21 21:20:25 +0100

    chore: Routing in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
 import Root from './routes/Root.jsx'
-import Header from './routes/Header.jsx';
-import Footer from './routes/Footer.jsx';
-import Homepage from './routes/Homepage.jsx';
 import Shopping from './routes/Shopping.jsx';
-import Itemcard from './routes/Itemcard.jsx';
+import ErrorPage from './routes/ErrorPage.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />
-  }
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+    path="/"
+    element={<Root />}
+    errorElement={<ErrorPage />}
+    >
+      <Route 
+      path="/Shopping"
+      element={<Shopping />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,3 +1,6 @@
+import Header from "./Header"
+import Footer from "./Footer"
+
 export default function Root() {
     return (
     <>
