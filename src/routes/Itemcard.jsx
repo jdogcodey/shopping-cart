990ee4da16bcfb56e504b80dca3a682690a8e06a
Jonathan Dawson <jonnydawson@hotmail.com>
2024-11-01 11:39:37 +0000

    feat: create itemcard component

--- src/routes/Itemcard.jsx
+++ src/routes/Itemcard.jsx
@@ -1,3 +1,9 @@
-export default function Itemcard() {
-    
+export default function Itemcard({ title, price, description, image, rating }) {
+    return <div>
+        <h3>{title}</h3>
+        <img src={image} />
+        <p>{description}</p>
+        <p>£{price}</p>
+        <p>{rating}/5</p>
+    </div>
 }
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer"
+import Sidebar from "./Sidebar"
 import { Outlet } from 'react-router-dom'
 
 export default function Root() {
     return (
     <>
         <Header />
+        <Sidebar />
         <Outlet />
         <Footer />
     </>
