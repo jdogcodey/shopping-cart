26f4ee550aa2820729b0599f139d59b44d60b4c2
Jonathan Dawson <jonnydawson@hotmail.com>
2024-11-01 11:01:32 +0000

    feat: Create homepage

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,3 +1,8 @@
-export default function Homepage() {
+import Link from 'react-router-dom';
 
+export default function Homepage() {
+    <main>
+        <p>Welcome!</p>
+        <Link to='/shopping'>Let&apos;s Shop!</Link>
+    </main>
 }
