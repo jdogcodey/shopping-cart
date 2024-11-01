c893c62b0f3e7ddacb8cceff884b9a92d6b08348
Jonathan Dawson <jonnydawson@hotmail.com>
2024-11-01 10:20:17 +0000

    Feat: Add footer with links to my Github and TOP course

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -1,3 +1,5 @@
 export default function Footer() {
-    
+    return <footer>
+        <p>Made by <a href='https://github.com/jdogcodey'>jdogcodey</a> - Following <a href='https://www.theodinproject.com/'>The Odin Project</a> <a href='https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart'>Shopping Cart Project</a></p>
+    </footer>
 }
--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -3,13 +3,13 @@ import githubLogo from '../assets/github-mark.svg';
 
 export default function Header() {
     return (
-        <>
+        <header>
             <h1>Shop Capitalism!</h1>
             <nav>
                 <NavLink to={'/'} className={({isActive, isPending}) => (isActive ? 'active' : isPending ? 'pending' : '')}>Home</NavLink>
                 <NavLink to={'/shopping'} className={({isActive, isPending}) => (isActive ? 'active' : isPending ? 'pending' : '')}>Shop</NavLink>
                 <a><img src={githubLogo} alt="Github Logo"/></a>
             </nav>
-        </>
+        </header>
     )
 }
