824b298de8150aff0af143d30825ea50251af847
Jonathan Dawson <jonnydawson@hotmail.com>
2024-11-22 18:09:52 +0000

    Checkout screen set up

--- src/routes/Checkout.jsx
+++ src/routes/Checkout.jsx
@@ -3,16 +3,25 @@ import { useOutletContext } from "react-router-dom";
 export default function Checkout() {
     const {shoppingCart, addToCart, removeFromCart, changeCountInCart, noInCart} = useOutletContext();
 
-    return <div>
-        <h2>Basket:</h2>
-        {shoppingCart.map((item) => 
-        <div key={item.id}>
-            <p>{item.title}</p>
-            <img src={item.image}></img>
-            <button onClick={() => {removeFromCart(item)}}>-</button>
-            <input value={noInCart(item)} onChange={(event) => changeCountInCart(event, item)}></input>
-            <button onClick={() => {addToCart(item)}}>+</button>
-        </div>)}
-        <button>Checkout</button>
+    return <>
+    <main className="flex justify-center items-center w-full">
+        <div className='flex flex-col bg-[#4a90a4] p-4 rounded-lg'>
+            <h2 className="text-white text-3xl">Basket:</h2>
+            {shoppingCart.map((item) => 
+            <div className='flex flex-row bg-white m-2 rounded-md p-2 justify-between' key={item.id}>
+                <div className="flex flex-row items-center mr-4">
+                    <img className="max-h-24 mr-2" src={item.image}></img>
+                    <p>{item.title}</p>
+                </div>
+                <div className="flex flex-row items-center">
+                    <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-2 pr-2 mr-2" onClick={() => {removeFromCart(item)}}>-</button>
+                    <input className='w-20 bg-[#dcefe7]' value={noInCart(item)} onChange={(event) => changeCountInCart(event, item)}></input>
+                    <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-2 pr-2 ml-2" onClick={() => {addToCart(item)}}>+</button>
+                </div>
+            </div>)}
+            <button className="text-white border-solid border-2 border-white rounded-md text-3xl hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]">Checkout</button>
         </div>
+        </main>
+    </>
+    
 }
