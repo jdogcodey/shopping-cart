5f4637d622ac4e1249f0c9100b1f1223a7ea9f9f
jdogcodey <jonnydawson@hotmail.com>
2024-12-02 15:27:29 +0000

    fix: move back button on mobile checkout

--- src/routes/Checkout.jsx
+++ src/routes/Checkout.jsx
@@ -5,8 +5,8 @@ export default function Checkout() {
     const {shoppingCart, addToCart, removeFromCart, changeCountInCart, noInCart} = useOutletContext();
 
     return <>
-    <Link to='/shopping' className="left-5 top-20 fixed text-white bg-[#4a90a4] p-2 text-2xl rounded-3xl border-solid border-white border-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]">Back</Link>
-    <main className="flex justify-center items-center w-full">
+    <Link to='/shopping' className="left-1 md:left-5 top-16 md:top-20 fixed text-white bg-[#4a90a4] p-2 text-2xl rounded-3xl border-solid border-white border-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]">Back</Link>
+    <main className="flex justify-center items-center w-full pt-20">
         <div className='flex flex-col bg-[#4a90a4] p-4 rounded-lg'>
             <h2 className="text-white text-3xl">Basket:</h2>
             {shoppingCart.map((item) => 
@@ -17,7 +17,7 @@ export default function Checkout() {
                 </div>
                 <div className="flex flex-row items-center">
                     <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-2 pr-2 mr-2" onClick={() => {removeFromCart(item)}}>-</button>
-                    <input className='w-20 bg-[#dcefe7]' value={noInCart(item)} onChange={(event) => changeCountInCart(event, item)}></input>
+                    <input className='w-8 md:w-20 bg-[#dcefe7]' value={noInCart(item)} onChange={(event) => changeCountInCart(event, item)}></input>
                     <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-2 pr-2 ml-2" onClick={() => {addToCart(item)}}>+</button>
                 </div>
             </div>)}
