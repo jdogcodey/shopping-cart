896e2e14381785ff89b7f963080650868d86d67e
jdogcodey <jonnydawson@hotmail.com>
2024-12-02 15:35:58 +0000

    fix: nav link height

--- src/routes/Checkout.jsx
+++ src/routes/Checkout.jsx
@@ -5,15 +5,15 @@ export default function Checkout() {
     const {shoppingCart, addToCart, removeFromCart, changeCountInCart, noInCart} = useOutletContext();
 
     return <>
-    <Link to='/shopping' className="left-1 md:left-5 top-16 md:top-20 fixed text-white bg-[#4a90a4] p-2 text-2xl rounded-3xl border-solid border-white border-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]">Back</Link>
+    <Link to='/shopping' className="left-1 md:left-5 top-16 md:top-20 fixed text-white bg-[#4a90a4] p-2 text-md md:text-2xl rounded-3xl border-solid border-white border-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]">Back</Link>
     <main className="flex justify-center items-center w-full pt-20">
-        <div className='flex flex-col bg-[#4a90a4] p-4 rounded-lg'>
-            <h2 className="text-white text-3xl">Basket:</h2>
+        <div className='flex flex-col bg-[#4a90a4] p-2 md:p-4 rounded-lg ml-2 mr-2'>
+            <h2 className="text-white text-lg md:text-3xl">Basket:</h2>
             {shoppingCart.map((item) => 
             <div className='flex flex-row bg-white m-2 rounded-md p-2 justify-between' key={item.id}>
                 <div className="flex flex-row items-center mr-4">
                     <img className="max-h-24 mr-2" src={item.image}></img>
-                    <p>{item.title}</p>
+                    <p className='text-sm md:text-lg'>{item.title}</p>
                 </div>
                 <div className="flex flex-row items-center">
                     <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-2 pr-2 mr-2" onClick={() => {removeFromCart(item)}}>-</button>
@@ -21,7 +21,7 @@ export default function Checkout() {
                     <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-2 pr-2 ml-2" onClick={() => {addToCart(item)}}>+</button>
                 </div>
             </div>)}
-            <button className="text-white border-solid border-2 border-white rounded-md text-3xl hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]">Checkout</button>
+            <button className="text-white border-solid border-2 border-white rounded-md text-lg md:text-3xl hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]">Checkout</button>
         </div>
         </main>
     </>
--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -6,8 +6,8 @@ export default function Header() {
         <header className="flex flex-row justify-between items-center p-3 h-16 bg-[#4a90a4] text-[#f5f9f7] fixed w-full">
             <Link to='/'><h1 className="sm:text-2xl lg:text-3xl items-center">Shop Capitalism!</h1></Link>
             <nav className="flex flex-row items-center h-10">
-                <NavLink to={'/'} className={({isActive, isPending}) => `mr-2 border-solid border-[#f5f9f7] border-2 p-1 rounded-3xl pl-2 pr-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6] ${isActive ? 'active' : isPending ? 'pending' : ''}`}>Home</NavLink>
-                <NavLink to={'/shopping'} className={({isActive, isPending}) => `mr-2 border-solid border-[#f5f9f7] border-2 p-1 rounded-3xl pl-2 pr-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6] ${isActive ? 'active' : isPending ? 'pending' : ''}`}>Shop</NavLink>
+                <NavLink to={'/'} className={({isActive, isPending}) => `text-sm md:text-base mr-2 border-solid border-[#f5f9f7] border-2 p-1 rounded-3xl pl-2 pr-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6] ${isActive ? 'active' : isPending ? 'pending' : ''}`}>Home</NavLink>
+                <NavLink to={'/shopping'} className={({isActive, isPending}) => `text-sm md:text-base mr-2 border-solid border-[#f5f9f7] border-2 p-1 rounded-3xl pl-2 pr-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6] ${isActive ? 'active' : isPending ? 'pending' : ''}`}>Shop</NavLink>
                 <a className="h-10 flex items-center align-middle" href="https://github.com/jdogcodey"><img className="h-8" src={githubLogo} alt="Github Logo"/></a>
             </nav>
         </header>
