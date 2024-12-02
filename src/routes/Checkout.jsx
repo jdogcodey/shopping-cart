dd3f6e6f57549a693b1544be826f73f2e59a639c
jdogcodey <jonnydawson@hotmail.com>
2024-12-02 16:04:42 +0000

    feat: scroll to top when basket opened

--- src/routes/Checkout.jsx
+++ src/routes/Checkout.jsx
@@ -6,7 +6,7 @@ export default function Checkout() {
 
     return <>
     <Link to='/shopping' className="left-1 md:left-5 top-16 md:top-20 fixed text-white bg-[#4a90a4] p-2 text-md md:text-2xl rounded-3xl border-solid border-white border-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]">Back</Link>
-    <main className="flex justify-center items-center w-full pt-20">
+    <main className="flex justify-center items-center w-full pt-28 mb-5">
         <div className='flex flex-col bg-[#4a90a4] p-2 md:p-4 rounded-lg ml-2 mr-2'>
             <h2 className="text-white text-lg md:text-3xl">Basket:</h2>
             {shoppingCart.map((item) => 
--- src/routes/Itemcard.jsx
+++ src/routes/Itemcard.jsx
@@ -23,23 +23,23 @@ export default function Itemcard({itemObject}) {
     }
 }
 
-    return <div className='border-solid border-[#4a90a4] border-2 rounded-md bg-white grid grid-cols-cardlayout grid-rows-cardlayout gap-2'>
-        <h3 className="bg-[#4a90a4] p-3 col-span-4 overflow-hidden text-white">{itemObject.title}</h3>
+    return <div className='border-solid border-[#4a90a4] border-2 rounded-md bg-white grid grid-cols-cardlayout grid-rows-cardlayout gap-2 text-xs md:text-base items-center'>
+        <h3 className="bg-[#4a90a4] p-3 col-span-4 overflow-hidden text-white max-h-[40px] line-clamp-1">{itemObject.title}</h3>
         <div className="flex align-center justify-center p-1 col-start-4 row-start-2 row-end-4">
-            <img className='max-h-full  max-w-full object-contain' src={itemObject.image} />
+            <img className='max-h-[140px]  max-w-full object-contain' src={itemObject.image} />
         </div>
-        <p className="row-start-2 col-start-1 col-end-4 row-end-3 p-2 overflow-scroll">{itemObject.description}</p>
+        <p className="row-start-2 col-start-1 col-end-4 row-end-3 p-2 overflow-scroll max-h-[140px]">{itemObject.description}</p>
         <p className="row-start-3 row-end-4 col-start-1 col-end-2 p-2 text-center">£{itemObject.price}</p>
         <p className='row-start-3 row-end-4 col-start-2 col-end-3 p-2 text-center'>{itemObject.rating.rate}/5</p>
         <div className='row-start-3 row-end-4 col-start-3 col-end-4 lg:p-2 text-center'>
             {itemInCart(itemObject) ?
                 <div className="flex flex-row">
-                    <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-2 pr-2 mr-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]" onClick={() => removeFromCart(itemObject)}>-</button>
-                    <input className='w-20 bg-[#dcefe7]' type="number" value={noInCart(itemObject)} onChange={(event) => changeCountInCart(event, itemObject)}></input>
+                    <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-1 md:pl-2 pr-1 md:pr-2 mr-1 md:mr-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]" onClick={() => removeFromCart(itemObject)}>-</button>
+                    <input className='w-10 md:w-20 bg-[#dcefe7]' type="number" value={noInCart(itemObject)} onChange={(event) => changeCountInCart(event, itemObject)}></input>
                     <button className="border-solid border-[#4a90a4] border-2 rounded-full pl-2 pr-2 ml-2 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]" onClick={() => {
                         addToCart(itemObject)}}>+</button>
                 </div> :
-                <button className='text-sm border-solid border-[#4a90a4] border-2 rounded-xl pl-1 pr-1 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]' onClick={() => {addToCart(itemObject)}}>Add to Basket</button>}
+                <button className='border-solid border-[#4a90a4] border-2 rounded-xl pl-1 pr-1 hover:border-[#7cb6c6] hover:bg-[#dcefe7] hover:text-[#7cb6c6]' onClick={() => {addToCart(itemObject)}}>Add to Basket</button>}
         </div>
     </div>
 }
